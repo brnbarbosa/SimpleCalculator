e6a80a019d3895f40c135029a327a22ab0ed42c8
brnbarbosa <bruno.ebarbosa@gmail.com>
2025-04-16 12:20:10 -0300

    Testing operations

--- calculator.py
+++ calculator.py
@@ -9,16 +9,22 @@ def main() -> None:
             first_number_str = input("Enter the first number of calculation: ")
             if first_number_str.startswith("-"):
                 first_number = float(first_number_str[1:]) * -1.0
-                print(sum_op(first_number))
             else:
                 first_number = float(first_number_str)
 
-            print(first_number)
+
+            second_number_str = input("Enter the second number of calculation: ")
+            if second_number_str.startswith("-"):
+                second_number = float(second_number_str[1:]) * -1.0
+            else:
+                second_number = float(second_number_str)
+
+            print(sum_op(first_number, second_number), sub_op(first_number, second_number), mult_op(first_number, second_number), div_op(first_number, second_number))
         except:
             print('Invalid input')
 
 
-def sum_op(first_number : float, second_number : float = 2.0) -> float:
+def sum_op(first_number : float, second_number : float) -> float:
     return first_number + second_number
 
 def sub_op(first_number : float, second_number : float) -> float:
@@ -26,6 +32,9 @@ def sub_op(first_number : float, second_number : float) -> float:
 
 def mult_op(first_number : float, second_number : float) -> float:
     return first_number * second_number
+
+def div_op(first_number : float, second_number : float) -> float:
+    return first_number / second_number
 
 if __name__ == "__main__":
     main()
