434651c7a5d4cf01e72938407f56d512f2ced3b9
brnbarbosa <bruno.ebarbosa@gmail.com>
2025-04-17 12:02:35 -0300

    Refactoring the getting user input and erro handling

--- calculator.py
+++ calculator.py
@@ -1,40 +1,67 @@
-def main() -> None:
-    first_number : float = 0.0
-    second_number : float = 0.0
-    result : float = 0.0
+def calculator() -> None:
+    first_operand : float = get_float_input("Enter the first operand of calculation: ")
+    second_operand : float = get_float_input("Enter the second operand of calculation: ")
+    operator : str = get_operator("Insert a valid operator (+, -, *, /): ")
 
+    calculation_result = result(first_operand, second_operand, operator)
 
+    if calculation_result != None:
+        print("Result: " + str(calculation_result))
+    
+def get_float_input(prompt : str) -> float:
     while True:
         try:
-            first_number_str = input("Enter the first number of calculation: ")
-            if first_number_str.startswith("-"):
-                first_number = float(first_number_str[1:]) * -1.0
-            else:
-                first_number = float(first_number_str)
+            operand_str = input(prompt)
+            operand = float(operand_str)
 
+            return operand
+        except ValueError:
+            print('Invalid Input')
 
-            second_number_str = input("Enter the second number of calculation: ")
-            if second_number_str.startswith("-"):
-                second_number = float(second_number_str[1:]) * -1.0
-            else:
-                second_number = float(second_number_str)
+def get_operator(prompt : str) -> str:
+    while True:
+        try:
+            operator = input(prompt)
+            valid_operators = ['+', '-', '*', '/']
+            for item in valid_operators:
+                if operator == item:
+                    return operator
+        except ValueError:
+            print("Invalid input.")
 
-            print(sum_op(first_number, second_number), sub_op(first_number, second_number), mult_op(first_number, second_number), div_op(first_number, second_number))
-        except:
-            print('Invalid input')
+def result(first_operand : float, second_operand : float, operator : str) -> float:
+    calculation_result = None
 
+    match operator:
+        case '+':
+            calculation_result = sum_op(first_operand, second_operand)
+        case '-':
+           calculation_result = sub_op(first_operand, second_operand)
+        case '*':
+            calculation_result = mult_op(first_operand, second_operand)
+        case '/':
+            calculation_result = div_op(first_operand, second_operand)
+        case _:
+            print("Error: Unknown operator")
 
-def sum_op(first_number : float, second_number : float) -> float:
-    return first_number + second_number
+    return calculation_result
 
-def sub_op(first_number : float, second_number : float) -> float:
-    return first_number - second_number
+def sum_op(first_operand : float, second_operand : float) -> float:
+    return first_operand + second_operand
 
-def mult_op(first_number : float, second_number : float) -> float:
-    return first_number * second_number
+def sub_op(first_operand : float, second_operand : float) -> float:
+    return first_operand - second_operand
 
-def div_op(first_number : float, second_number : float) -> float:
-    return first_number / second_number
+def mult_op(first_operand : float, second_operand : float) -> float:
+    return first_operand * second_operand
+
+def div_op(first_operand : float, second_operand : float) -> float:
+    try:
+        result = first_operand / second_operand
+        return result
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+        return None
 
 if __name__ == "__main__":
-    main()
+    calculator()
